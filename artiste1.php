10f61ce6eb02418c461a838e211ba269b4e36f82
SarahRubio <sarahrubio.pro@gmail.com>
2020-05-26 18:15:15 +0200

    modification de la page artiste1

--- artiste1.php
+++ artiste1.php
@@ -3,6 +3,11 @@
 include "config.php";
 include "contenu.php";
 
-$artiste = $contenuSite["artiste1"];
-
-include $_dossier_template  . "artisteDetail.php";
+if (isset($contenuSite[$_GET["artisteChoisi"]])) {
+  $artiste = $contenuSite[$_GET["artisteChoisi"]];
+  include $_dossier_template  . "artisteDetail.php";
+}
+else {
+  header("Location: $_url_base");
+  exit();
+}
